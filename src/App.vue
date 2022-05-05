cd8857332e4058a0d1074a00aa4e3347faeb5bb8
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-05 20:27:58 +0300

    Conected TheHeader component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  test
+  <div>
+    <the-header></the-header>
+  </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 export default {
+  components: {
+    TheHeader,
+  }
 }
 </script>
 
