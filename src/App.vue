b085ea4717d6ffc1a2bd9d833fc4396d90a92154
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-05 20:39:01 +0300

    Conected UserInfo ind App and created data

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,32 @@
   <div>
     <the-header></the-header>
     <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
 export default {
   components: {
     TheHeader,
     BadgeList,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
   },
 };
 </script>
