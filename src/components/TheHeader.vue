5d941ea9bb91b8001a09a75ce616a7725527ab6e
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-05-05 20:17:39 +0300

    Addid TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+      <h1>More on Vue Components</h1>
+  </header>
+</template>
+
+<style scoped>
+    header {
+        width: 100%;
+        height: 5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #14005e;
+    }
+    header {
+        color: white;
+        margin: 0;
+    }
+</style>
